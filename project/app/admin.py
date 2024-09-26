eab522ff0e5cc5dc0bc64f408dea4e05e4e24e45
Bohdansuskyi <bohdansyskiy1214@gmail.com>
2024-09-26 17:23:50 +0200

    created database with superuser and admin panel

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import get_information, send_information
 
-# Register your models here.
+admin.site.register(get_information)
+admin.site.register(send_information)
--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# models for creating database
+
+class get_information(models.Model):
+    get_value = models.BooleanField()
+    time_get = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+class send_information(models.Model):
+    send_value = models.BooleanField()
+    time_send = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
